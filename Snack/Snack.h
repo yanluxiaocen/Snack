4f9a7885c07297a2c67ebe9f4151732000241a2d
yanluxiaocen <formulatree@qq.com>
2026-09-02 23:58:57 +0800

    1

--- Snack/Snack.h
+++ Snack/Snack.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <deque>
+#include"Point.h"
+
+class Snack {
+private:
+	std::deque<Point> m_body;
+	int m_direction;
+	bool m_isGrow;
+
+public:
+	Snack(int startX, int startY);
+	void move();
+	void grow();
+	bool isHeadAt(const Point& pos) const;
+
+	const std::deque<Point>& getBody() const;
+	int getDirection() const;
+};
